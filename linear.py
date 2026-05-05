84a9aeb40c9c1ef003f4ee3b5cf44bcd73adbafc
hanna20251230-pixel <hanna20251230@miis.ae>
2026-05-05 11:38:36 +0400

    Update linear.py

--- linear.py
+++ linear.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 df=pd.read_csv("student_scores.csv")
-X=df[["Hours Studied"]]
+X=df[["HoursStudied"]]
 y=df["ExamScore"]
 X_train,X_test,y_train,y_test=train_test_split(X,y, test_size=0.2,random_state=42)
 model=LinearRegression()
